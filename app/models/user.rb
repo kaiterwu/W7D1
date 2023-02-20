e0192b7a4299c375807ab13f12d5c65426ba622b
andy <morrison.andrew422@gmail.com>
2023-02-20 15:11:21 -0500

    create controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,9 @@
 class User < ApplicationRecord
     validates :username, uniqueness: true, presence: true 
     validates :password_digest, presence: true 
-    before_validation
+    validates :password, length: { minimum: 6 }, allow_nil: true
+
+    before_validation :ensure_session_token
     
     attr_reader :password 
 
@@ -36,9 +38,13 @@ class User < ApplicationRecord
     end 
 
     def reset_session_token!
-
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
     end 
+
     private
+
     def generate_unique_session_token 
         token = SecureRandom::urlsafe_base64
         while User.exists?(session_token:token)
@@ -48,6 +54,7 @@ class User < ApplicationRecord
     end 
 
     def ensure_session_token
+        self.session_token ||= generate_unique_session_token
 
     end 
 end
